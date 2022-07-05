8b3690a31f8b8c33be6fafb89cd88039406e0a8e
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-07-05 23:25:12 +0530

    added entropy and observe methods

--- cell.py
+++ cell.py
@@ -6,10 +6,21 @@ class Cell:
         self.x = x
         self.y = y
         self.rez = rez
-        self.options = [random.choice(options)]
+        self.options = options
+        self.collapsed = False
 
     def draw(self, win):
         if len(self.options) == 1:
             self.options[0].draw(win, self.x * self.rez, self.y * self.rez)
         else:
             pygame.draw.rect(win, (100, 100, 100), (self.x * self.rez, self.y * self.rez, self.rez, self.rez), 2)
+
+    def entrophy(self):
+        return len(self.options)
+
+    def update(self):
+        self.collapsed = bool(self.entrophy() == 1)
+
+    def observe(self):
+        self.options = [random.choice(self.options)]
+        self.collapsed = True

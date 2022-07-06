a7c0f00e87734f64acfbf4400fc5754169ce8dc0
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2022-07-06 15:50:42 +0530

    added fonts

--- cell.py
+++ cell.py
@@ -1,6 +1,9 @@
 import pygame
 import random
 
+pygame.init()
+font = pygame.font.Font("./assets/custom_fonts/Poppins/Poppins-Light.ttf", 16)
+
 class Cell:
     def __init__(self, x, y, rez, options):
         self.x = x
@@ -9,11 +12,15 @@ class Cell:
         self.options = options
         self.collapsed = False
 
-    def draw(self, win):
+    def draw(self, win):        
         if len(self.options) == 1:
-            self.options[0].draw(win, self.x * self.rez, self.y * self.rez)
+            self.options[0].draw(win, self.y * self.rez, self.x * self.rez)
+            
         else:
-            pygame.draw.rect(win, (100, 100, 100), (self.x * self.rez, self.y * self.rez, self.rez, self.rez), 2)
+            pygame.draw.rect(win, (0, 0, 0), (self.y * self.rez, self.x * self.rez, self.rez, self.rez))
+
+        cell_entrophy = font.render(f"{self.entropy()} {(self.x, self.y)}", 1, (255, 255, 255))
+        win.blit(cell_entrophy, ((self.y * self.rez) + (self.rez // 2 - 18), (self.x * self.rez) + (self.rez // 2 - 18)))
 
     def entropy(self):
         return len(self.options)
